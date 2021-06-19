c82a0cb321266454ba54ac7603489c0847f3e5a2
Eric Syu <xeriok02390@gmail.com>
2021-06-19 18:07:01 +0800

    add foreman and tailwindcss and rename app/javascript to app/frontend

---
+++ app/frontend/js/index.js
@@ -0,0 +1,2 @@
+import './sudoo';
+import './tictac';
--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -6,9 +6,8 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-
-require('../games/tictac')
-require('../games/sudoo')
+import "../stylesheets";
+import "../js";
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
